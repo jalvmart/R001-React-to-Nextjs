dcbdf2dcc7265521da13d9d1a0fd3f94f5e36976
Juan Alvaro Martinez <juanalvaromartinez@gmail.com>
2022-06-03 19:59:25 -0430

    Paso 2: Agregar Contenido: uso de funciones y ciclos

--- scr/index.js
+++ scr/index.js
@@ -1,9 +1,30 @@
 const app = document.getElementById("app")
 
+function Header({ title }) {
+    return <h1>{title ? title : "Default title"}</h1>
+}
+
 function HomePage() {
+    const skills = ["HTML", "CSS", "JavaScript"]
     return (
-        <h1>React</h1>
+        <div>
+            <Header title="Aprende React" />
+            <p>
+                React es una biblioteca de JavaScript basada en componentes.
+                Sirve para crear eficientes interfaces de usuario (UI).
+            </p>
+            <p>
+                Para utilizar la biblioteca de React es recomendable dominar:
+            </p>
+            <ul>
+                {skills.map((sk) =>
+                    <li key={sk}>{sk}</li>
+                )}
+            </ul>            
+        </div>
     )
+
+
 }
 
 ReactDOM.render(<HomePage />, app)
